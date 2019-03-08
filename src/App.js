2442b67cc12a5ef5e34c50a76ed70e195a8b80aa
kpwwalsh <wnuru@hotmail.com>
2019-03-08 10:36:38 -0600

    Chatrooms not showing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+import RoomList from './components/RoomList';
+
+  var config = {
+    apiKey: "AIzaSyAX0XklFlFeiqQ_Rsw88_iuGpPnvdy6UH0",
+    authDomain: "bloc-chat-ba747.firebaseapp.com",
+    databaseURL: "https://bloc-chat-ba747.firebaseio.com",
+    projectId: "bloc-chat-ba747",
+    storageBucket: "bloc-chat-ba747.appspot.com",
+    messagingSenderId: "845660248798"
+  };
+  firebase.initializeApp(config);
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+   <RoomList firebase={firebase}/>
       </div>
     );
   }
