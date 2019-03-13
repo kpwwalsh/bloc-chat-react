a868a94d2d455d21a72dc1a3242cd460a0ae008a
kpwwalsh <wnuru@hotmail.com>
2019-03-12 23:33:40 -0500

    Create rooms

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -1,14 +1,16 @@
 import React, {Component} from 'react';
  
-
-
 class RoomList extends Component {
     constructor(props){
         super(props)
         this.state = {
-            rooms: []
+            rooms: [],
+            name: "",
+            newRoomName: ''
           };
         this.roomsRef = this.props.firebase.database().ref('rooms');
+        this.createRoom = this.createRoom.bind(this);
+        this.handleChange = this.handleChange.bind(this);
      }
    
     componentDidMount() {
@@ -19,19 +21,45 @@ class RoomList extends Component {
         });
       } 
 
+      createRoom(e){
+        this.roomsRef.push({
+          name: this.state.newRoomName
+        });
+        this.setState({name:""});
+      }
 
+      handleChange(e){
+        const newName= e.target.value;
+        this.setState({
+        newRoomName: newName  
+        })
+      }
       
       render(){ 
         return( 
+          <div>
           <section>
-        {this.state.rooms.map((room, index) => 
-         <li key = {index}>{room.name}</li> 
-           ) 
-          }
-         </section>
+           {this.state.rooms.map((room, index) => 
+           <li key = {index}>{room.name}</li> 
+            ) 
+            }
+          </section>
+           <section>
+            <form id="create-room" onSubmit={(e)=> this.createRoom(e)}>
+             <input type="text"
+              name="name"
+              placeholder="add room"
+              value={this.state.newRoomName}
+              onChange={(e)=> this.handleChange(e)}
+             />
+             <input type="submit"/>
+           </form>
+           </section>    
+           </div> 
          );
         }
       }
+      
   
     export default RoomList;
 
