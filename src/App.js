c3954697dffc9b83625da482c15aad3cbd8702c7
kpwwalsh <wnuru@hotmail.com>
2019-03-19 13:26:02 -0500

    Checkpoint list messages, not working

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import * as firebase from 'firebase';
 import RoomList from './components/RoomList';
-import * as uuid from 'uuid';
+import MessageList from './components/MessageList';
 
   var config = {
     apiKey: "AIzaSyAX0XklFlFeiqQ_Rsw88_iuGpPnvdy6UH0",
@@ -16,13 +16,31 @@ import * as uuid from 'uuid';
 
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      activeRoom:{key:0, name:"" }
+    }
+    this.setActiveRoom= this.setActiveRoom.bind(this);
+  };
+
+  setActiveRoom(){
+    this.setState.activeRoom({activeRoom:"room"})
+  }
+
   render() {
     return (
       <div className="App">
-   <RoomList firebase={firebase}/>
+        <RoomList firebase={firebase}
+          setActiveRoom={function(newActiveRoom) {
+            this.setState({ activeRoom: newActiveRoom });
+          }.bind(this)}
+        />
+        <MessageList firebase={firebase} activeRoom={this.state.activeRoom} />
       </div>
     );
   }
 }
 
 export default App;
+
